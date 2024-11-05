8e1b02dd5badedc6c75d800a7333838e24a0379d
squaresweet <svetlana120595@gmail.com>
2024-11-05 13:59:07 +0300

    Spring template

---
+++ src/main/java/me/sqsw/buildandroll/BuildandrollApplication.java
@@ -0,0 +1,13 @@
+package me.sqsw.buildandroll;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuildandrollApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuildandrollApplication.class, args);
+	}
+
+}
---
+++ src/test/java/me/sqsw/buildandroll/BuildandrollApplicationTests.java
@@ -0,0 +1,13 @@
+package me.sqsw.buildandroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuildandrollApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
