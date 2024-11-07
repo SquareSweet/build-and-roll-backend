be787e9592ac772940b1ed55bdaf228b71955ccb
IDante <maxim.polyanskiy@mail.ru>
2024-11-07 19:37:15 +0300

    feat: add characterListId to USER_JOIN event

--- src/main/java/me/sqsw/buildandroll/dto/room/UserJoinToRoomDto.java
+++ src/main/java/me/sqsw/buildandroll/dto/room/UserJoinToRoomDto.java
@@ -1,8 +1,10 @@
 package me.sqsw.buildandroll.dto.room;
 
+import jakarta.annotation.Nullable;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
+import me.sqsw.buildandroll.dto.UserDto;
 import me.sqsw.buildandroll.model.User;
 
 import java.util.List;
@@ -12,8 +14,17 @@ import java.util.Objects;
 @Setter
 @AllArgsConstructor
 public class UserJoinToRoomDto {
-    private User user;
+    private UserDto user;
     private Long roomId;
+
+    public UserJoinToRoomDto(User user, Long roomId) {
+        this.user = new UserDto(user);
+        this.roomId = roomId;
+    }
+
+    public void setCharacterListId(Long characterListId) {
+        this.user.setCharacterListId(characterListId);
+    }
 
     public List<Object> toList(){
         return List.of(user, roomId);
