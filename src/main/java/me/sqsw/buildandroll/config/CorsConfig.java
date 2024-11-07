4568b7421584f2cd6c8f3474fd7edebae4af4879
IDante <maxim.polyanskiy@mail.ru>
2024-11-08 00:09:40 +0300

    fix: deploy

--- src/main/java/me/sqsw/buildandroll/config/CorsConfig.java
+++ src/main/java/me/sqsw/buildandroll/config/CorsConfig.java
@@ -14,7 +14,7 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+        config.addAllowedOrigin("http://localhost:8079");
         config.addAllowedMethod("*");
         config.addAllowedHeader("*");
         source.registerCorsConfiguration("/**", config);
